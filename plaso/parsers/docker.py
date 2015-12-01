c09d3bfdb9ff660a494b0f19dd83ffdb28d32eb0
Romain Gayon <romaing@google.com>
2015-12-01 14:31:35 +0100

    poil

--- plaso/parsers/docker.py
+++ plaso/parsers/docker.py
@@ -37,16 +37,18 @@ class DockerJSONParser(interface.FileObjectParser):
     file_object.seek(0, os.SEEK_SET)
 
 
+    # We need the path to the file to know what it describes
     try:
       json_file_path = parser_mediator.GetFileEntry().path_spec.location
     except AttributeError as exception:
+      # This happens on compressed JSON files
       json_file_path = parser_mediator.GetFileEntry().path_spec.parent.location
     try:
       if json_file_path.find("/containers")> 0 :
+        # TODO: parse json.log
         if json_file_path.find("/config.json") >0:
           self._ParseContainerConfigJSON(file_object, parser_mediator)
-        elif json_file_path.find("/hostconfig.json")>0:
-          pass
+
       elif json_file_path.find("/graph")> 0 :
         if json_file_path.find("/json") >0:
           self._ParseLayerConfigJSON(file_object, parser_mediator)
@@ -77,7 +79,7 @@ class DockerJSONParser(interface.FileObjectParser):
       return
     if "created" in j:
       ts=self._GetDateTimeFromString(j["created"])
-      attr["action"]="Layer Created"
+      attr["command"]=" ".join([ x.strip() for x in j["container_config"]["Cmd"]])
       parser_mediator.ProduceEvent(DockerJSONLayerEvent(ts,eventdata.EventTimestamp.ADDED_TIME,attr))
 
 
@@ -111,7 +113,6 @@ class DockerJSONEvent(time_events.TimestampEvent):
 
   def __init__(self, timestamp, event_type,attributes):
     super(DockerJSONEvent, self).__init__(timestamp, event_type)
-    self.action=attributes['action']
     self.id_=attributes['id']
 
 
@@ -119,12 +120,18 @@ class DockerJSONContainerEvent(DockerJSONEvent):
   """Event for stuff parsed from Containers' config.json files."""
 
   DATA_TYPE = u'docker:json:container'
+  def __init__(self, timestamp, event_type,attributes):
+    super(DockerJSONContainerEvent, self).__init__(timestamp, event_type,attributes)
+    self.action=attributes['action']
 
 
 class DockerJSONLayerEvent(DockerJSONEvent):
   """ Event for stuff from graph/**/json """
 
   DATA_TYPE = u'docker:json:layer'
+  def __init__(self, timestamp, event_type,attributes):
+    super(DockerJSONLayerEvent, self).__init__(timestamp, event_type,attributes)
+    self.command=attributes['command']
 
 manager.ParsersManager.RegisterParsers([
     DockerJSONParser,
