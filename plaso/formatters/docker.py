2bcc437ec12f4be1bf175e8b7bf1e69b8a2c8679
Romain Gayon <romaing@google.com>
2015-12-01 14:31:35 +0100

    poil

--- plaso/formatters/docker.py
+++ plaso/formatters/docker.py
@@ -11,7 +11,6 @@ class DockerEventFormatter(interface.ConditionalEventFormatter):
   DATA_TYPE = u'docker:json'
 
   FORMAT_STRING_SHORT_PIECES = [
-      u'{action}',
       u'{id}']
 
   SOURCE_SHORT = u'DOCKER'
@@ -22,9 +21,14 @@ class DockerLayerEventFormatter(
 
   DATA_TYPE = u'docker:json:layer'
 
-  FORMAT_STRING_PIECES = [
+  FORMAT_STRING_PIECES= (
       u'Layer ID: {id}',
-      u'Action: {action}']
+      u'Command: {cmd}'
+  )
+  FORMAT_STRING_SHORT_PIECES= (
+      u'ID: {id}',
+      u'Cmd: {cmd}'
+  )
 
   SOURCE_LONG = u'Docker Layer Event'
   SOURCE_SHORT = u'DOCKER'
