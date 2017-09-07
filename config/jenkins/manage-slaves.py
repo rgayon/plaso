da6d594c5f91c2b0f247780f4cbcdfa5781186f4
Romain Gayon <romaing@google.com>
2017-09-07 17:03:34 +0200

    lol

--- config/jenkins/manage-slaves.py
+++ config/jenkins/manage-slaves.py
@@ -17,12 +17,9 @@ from oauth2client import client as oauth2client
 class SlaveManager(object):
   """Class for managing Jenkins Slaves."""
 
-  DEFAULT_DISKSIZE_GB = 200
-  DEFAULT_MACHINETYPE = 'n1-standard-8'
-  DEFAULT_NETWORK = 'default'
   DEFAULT_SCOPES = ['https://www.googleapis.com/auth/devstorage.read_write']
 
-  def __init__(self,  project, zone=None, debug=False):
+  def __init__(self, project, zone=None, debug=False):
     """Create a new SlaveManager."""
 
     self._debug = debug
@@ -48,7 +45,7 @@ class SlaveManager(object):
     """Waits for an API operation to complete."""
     while True:
       result = self._client.zoneOperations().get(
-          project = self._project, zone=self._zone, operation=operation['name']
+          project=self._project, zone=self._zone, operation=operation['name']
       ).execute()
       if result['status'] == 'DONE':
         if 'error' in result:
@@ -82,8 +79,7 @@ class SlaveManager(object):
           device_name, source_disk = pd.split(':', 1)
         source_url = (
             'https://www.googleapis.com/compute/v1/projects/{0:s}/zones/{1:s}/'
-            'disks/{2:s}').format(
-                          self._project, self._zone, source_disk)
+            'disks/{2:s}').format(self._project, self._zone, source_disk)
         disk_list.append(
             {
                 'deviceName': device_name,
@@ -94,39 +90,31 @@ class SlaveManager(object):
     return disk_list
 
   def CreateInstance(
-      self, instance_name, persistent_disks=None, disk_size=None, image=None, machinetype=None,
-      metadata=None, network=None, scopes=None):
+      self, instance_name, disk_size=None, source_image=None, machinetype=None,
+      metadata=None, network=None, persistent_disks=None, scopes=None):
     """Creates a GCE instance.
 
     Args:
         instance_name (str): the name to give to the slave.
-        persistent_disks (list(str)): list of strings describing the disks to attach
-          to the instance (Read-Only)
         disk_size (int): the size of the system disk, in GB. Must be larger than
           the image size.
-        image (str): the disk image to use. Can be either the name of an image
-          in the current project, or a complete image path (starting with
-          '/projects/<project_name>/zones...')
+        source_image (str): the path to the disk image to use.
+          Must be in the form: '/projects/<project_name>/zones/images/...')
         machinetype (str): the type of the machine to use (default:
           n1-standard-8).
         metadata (dict): optional metadata to set for the instance.
         network (str): type of network to use (default: 'default')
+        persistent_disks (list(str)): list of strings describing the disks to
+          attach to the instance (Read-Only)
         scopes (list[str]): the list of scopes to set for the instance.
     """
 
-    disk_size = disk_size or self.DEFAULT_DISKSIZE_GB
-    machinetype = machinetype or self.DEFAULT_MACHINETYPE
-    network = network or self.DEFAULT_NETWORK
     scopes = scopes or self.DEFAULT_SCOPES
-    source_image = image
 
     self._Log('Creating new instance {0:s}'.format(instance_name))
 
     project_url = 'compute/v1/projects/{0:s}'.format(self._project)
     zone_url = '%s/zones/%s' % (project_url, self._zone)
-    if not source_image.startswith('/projects/'):
-      source_image = '/projects/{0:s}/global/images/{1:s}'.format(
-          self._project, source_image)
     machine_type_url = '{0:s}/zones/{1:s}/machineTypes/{2:s}'.format(
           project_url, self._zone, machinetype)
     network_url = '{0:s}/global/networks/{1:s}'.format(project_url, network)
@@ -180,65 +168,83 @@ class SlaveManager(object):
 
 
 if __name__ == '__main__':
-  parser = argparse.ArgumentParser()
-  parser.add_argument('action', choices=['create'])
-  parser.add_argument('--attach_persistent_disk', required=False, action='append')
-  parser.add_argument('--debug', required=False, action='store_true')
-  parser.add_argument('--disk_size', required=False, type=int)
-  parser.add_argument('--instance_name', required=False)
-  parser.add_argument('--image', required=True)
-  parser.add_argument('--linux_startup_script_url', required=False)
-  parser.add_argument('--machine_type', required=False)
-  parser.add_argument('--network', required=False)
-  parser.add_argument('--project', required=True)
-  parser.add_argument('--ssh_pub_key', required=False, action='append')
-  parser.add_argument('--windows_startup_script_url', required=False)
-  parser.add_argument('--zone', required=True)
+  parser = argparse.ArgumentParser(
+      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+  parser.add_argument(
+      '--attach_persistent_disk', action='append', required=False,
+      help='Select Persistent Disks to attach. ie: evidences-images')
+  parser.add_argument(
+      '--debug', required=False, action='store_true',
+      help='Print debug information')
+  parser.add_argument(
+      '--disk_size', action='store', required=False, default=200, type=int,
+      help='Boot disk size, in GB')
+  parser.add_argument(
+      '--instance_name', action='store', required=True, help='Name of instance')
+  parser.add_argument(
+      '--source_image', action='store', required=True,
+      help='Path to the image, ie: /projects/<project_name>/zones/images/...')
+  parser.add_argument(
+      '--linux_startup_script_url', action='store', required=False,
+      help='GCS url to a startup script for a Linux slave')
+  parser.add_argument(
+      '--machine_type', action='store', required=False, default='n1-standard-8',
+      help='Type of machine')
+  parser.add_argument(
+      '--network', action='store', required=False, default='default',
+      help='Type of network to use')
+  parser.add_argument(
+      '--project', action='store', required=True, help='Name of the project')
+  parser.add_argument(
+      '--ssh_pub_key', action='append', required=False,
+      help=('Specify SSH public keys to use. '
+            'Example: \'root:ssh-rsa AAAA... root\''))
+  parser.add_argument(
+      '--windows_startup_script_url', action='store', required=False,
+      help='GCS url to a startup script for a Windows slave')
+  parser.add_argument(
+      '--zone', action='store', required=True, help='The zone for the instance')
 
   flags = parser.parse_args(sys.argv[1:])
 
   instance_metadata = None
 
-  manager = SlaveManager(project=flags.project, zone=flags.zone, debug=flags.debug)
-  instance_metadata = {
-      'items': []
-  }
+  manager = SlaveManager(
+      project=flags.project, zone=flags.zone, debug=flags.debug)
 
-  if flags.action == 'create':
-    if not flags.instance_name:
-      parser.error('Creating a new Slave requires --instance_name to be specified')
+  instance_metadata = {'items': []}
 
-    if flags.windows_startup_script_url:
-      startup_item = {
-          'key': 'windows-startup-script-url',
-          'value': flags.windows_startup_script_url
-      }
-      instance_metadata['items'].append(startup_item)
+  if flags.windows_startup_script_url:
+    startup_item = {
+        'key': 'windows-startup-script-url',
+        'value': flags.windows_startup_script_url
+    }
+    instance_metadata['items'].append(startup_item)
 
-    if flags.linux_startup_script_url:
-      startup_item = {
-          'key': 'startup-script-url',
-          'value': flags.linux_startup_script_url
-      }
-      instance_metadata['items'].append(startup_item)
+  if flags.linux_startup_script_url:
+    startup_item = {
+        'key': 'startup-script-url',
+        'value': flags.linux_startup_script_url
+    }
+    instance_metadata['items'].append(startup_item)
 
-    if flags.ssh_pub_key:
-      ssh_key_item = {
-        'key': 'ssh-keys',
-        'value': '\n'.join(flags.linux_startup_script_url)
+  if flags.ssh_pub_key:
+    ssh_key_item = {
+      'key': 'ssh-keys',
+      'value': '\n'.join(flags.linux_startup_script_url)
 
-      }
-      instance_metadata['items'].append(ssh_key_item)
+    }
+    instance_metadata['items'].append(ssh_key_item)
 
-    try:
-      manager.CreateInstance(
-        flags.instance_name, persistent_disks=flags.attach_persistent_disk, image=flags.image, machinetype=flags.machine_type,
-        metadata=instance_metadata, network=flags.network)
-    except googleapiclient.errors.HttpError as error:
-      if error.resp['status'] == '409':
-        error_message = (
-            'There is already an instance names {0:s} in project {1:s}'.format(
-                flags.instance_name, flags.project)
-        )
-        print(error_message)
-        sys.exit
+  try:
+    manager.CreateInstance(
+      flags.instance_name, persistent_disks=flags.attach_persistent_disk,
+      source_image=flags.source_image, machinetype=flags.machine_type,
+      metadata=instance_metadata, network=flags.network)
+  except googleapiclient.errors.HttpError as error:
+    if error.resp['status'] == '409':
+      error_message = (
+          'There is already an instance names {0:s} in project {1:s}'.format(
+              flags.instance_name, flags.project)
+      )
+      print(error_message)
