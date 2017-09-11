0d914e315241724f9c5223af757a23ed08f7f826
Romain Gayon <romaing@google.com>
2017-09-11 13:51:26 +0200

    comments

--- config/jenkins/start_slave.py
+++ config/jenkins/start_slave.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 
 import argparse
 import datetime
+import json
 import sys
 import time
 
@@ -17,30 +18,36 @@ class SlaveManager(object):
 
   DEFAULT_SCOPES = ['https://www.googleapis.com/auth/devstorage.read_write']
 
-  def __init__(self, project, zone=None, debug=False):
-    """Create a new SlaveManager."""
+  def __init__(self, project, zone=None):
+    """Create a new SlaveManager.
 
-    self._debug = debug
+    Args:
+      project(str): the GCE project name.
+      zone(str): the destination GCP zone.
+    """
+
     self._project = project
     self._zone = zone
 
     self._client = self._CreateComputeClient()
 
-  def _Debug(self, msg):
-    """Prints a message if debug is set."""
-    if self._debug:
-      self._Log(msg)
-
-  def _Log(self, msg):
-    """Prints a message with timestamp."""
-    print '[{0:%c}] {1:s}'.format(datetime.datetime.now(), msg)
-
   def _CreateComputeClient(self):
-    """Creates an API client to do compute operations with."""
+    """Creates an API client to do compute operations with.
+
+    Returns:
+      Resource: an object with methods for interacting with the service.
+    """
     return discovery.build('compute', 'v1')
 
   def _WaitForOperation(self, operation):
-    """Waits for an API operation to complete."""
+    """Waits for an API operation to complete.
+
+    Args:
+      operation(dict): the API request.
+
+    Returns:
+      dict: the API call response.
+    """
     while True:
       result = self._client.zoneOperations().get(
           project=self._project, zone=self._zone, operation=operation['name']
@@ -51,25 +58,23 @@ class SlaveManager(object):
         return result
       time.sleep(1)
 
-  def _MakeAttachPD(self, persistent_disks):
+  def _BuildPersistentDiskList(self, persistent_disks):
     """Builds a list of dicts describing all disks to attach.
 
     Args:
-      persistent_disks(list(str)): the attached disks passed as argument.
+      persistent_disks (dict(str:str)]): list of disks to attach, in the form
+        {'persistent_disk_name': 'device_name'}.
+
     Returns:
       list(dict): the list of disks to attach.
     """
     disk_list = list()
     mode = 'READ_ONLY'
     if persistent_disks:
-      for pd in persistent_disks:
-        source_disk = pd
-        device_name = pd
-        if pd.find(':') > 0:
-          device_name, source_disk = pd.split(':', 1)
+      for pd_name, device_name in persistent_disks.items():
         source_url = (
             'https://www.googleapis.com/compute/v1/projects/{0:s}/zones/{1:s}/'
-            'disks/{2:s}').format(self._project, self._zone, source_disk)
+            'disks/{2:s}').format(self._project, self._zone, pd_name)
         disk_list.append(
             {
                 'deviceName': device_name,
@@ -85,23 +90,24 @@ class SlaveManager(object):
     """Creates a GCE instance.
 
     Args:
-        instance_name (str): the name to give to the slave.
-        disk_size (int): the size of the system disk, in GB. Must be larger than
-          the image size.
-        source_image (str): the path to the disk image to use.
-          Must be in the form: '/projects/<project_name>/zones/images/...')
-        machinetype (str): the type of the machine to use (default:
-          n1-standard-8).
-        metadata (dict): optional metadata to set for the instance.
-        network (str): type of network to use (default: 'default')
-        persistent_disks (list(str)): list of strings describing the disks to
-          attach to the instance (Read-Only)
-        scopes (list[str]): the list of scopes to set for the instance.
+        instance_name (str): the name to give to the instance.
+        disk_size (Optional[int]): the size of the system disk, in GB.
+          Must be larger than the image size.
+        source_image (Optional[str]): the path to the disk image to use.
+          Must be in the form: '/projects/<project_name>/zones/images/...'])
+        machinetype (Optional[str]): the type of the machine to use.
+          For a list of valid values, see:
+          https://cloud.google.com/compute/docs/machine-types
+        metadata (Optional[dict]): optional metadata to set for the instance.
+        network (Optional[str]): type of network to use (default: 'default')
+        persistent_disks (Optional[dict(str:str)]): list of disks to attach to
+          the instance, in the form {'persistent_disk_name': 'device_name'}.
+        scopes (Optional[list[str]]): the list of scopes to set for the instance.
     """
 
     scopes = scopes or self.DEFAULT_SCOPES
 
-    self._Log('Creating new instance {0:s}'.format(instance_name))
+    print 'Creating new instance {0:s}'.format(instance_name)
 
     project_url = 'compute/v1/projects/{0:s}'.format(self._project)
     machine_type_url = '{0:s}/zones/{1:s}/machineTypes/{2:s}'.format(
@@ -122,7 +128,7 @@ class SlaveManager(object):
         }
     ]
 
-    persistent_disks = self._MakeAttachPD(persistent_disks)
+    persistent_disks = self._BuildPersistentDiskList(persistent_disks)
     for persistent_disk in persistent_disks:
       disks.append(persistent_disk)
 
@@ -142,55 +148,52 @@ class SlaveManager(object):
     if metadata:
       instance_dict['metadata'] = metadata
 
-    self._Debug(instance_dict)
-
     operation = self._client.instances().insert(
         project=self._project, body=instance_dict, zone=self._zone).execute()
     self._WaitForOperation(operation)
 
-  def UpdateInstanceMetadata(self, instance_name, new_metadata):
-    """Update the instance metadata."""
-    operation = self._client.instances().setMetadata(
-        instance=instance_name, project=self._project, zone=self._zone,
-        body=new_metadata)
-    return operation.execute()
-
 
 if __name__ == '__main__':
-  parser = argparse.ArgumentParser(
-      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+  parser = argparse.ArgumentParser()
   parser.add_argument(
       '--attach_persistent_disk', action='append', required=False,
-      help='Select Persistent Disks to attach. ie: evidences-images')
+      metavar=('PERSISTENT_DISK'), help=(
+          'Attach PERSISTENT_DISK to the instance (ie: "evidence-images"). '
+          'It will be attached as /dev/disk/by-id/google-PERSISTENT_DISK')
+  )
   parser.add_argument(
-      '--debug', required=False, action='store_true',
-      help='Print debug information')
+      '--attach_persistent_disk_with_name', action='append', required=False,
+      metavar=('PERSISTENT_DISK:DEVICE_NAME'), help=(
+          'Attach PERSISTENT_DISK to the instance (ie: "evidence-images"). '
+          'It will be attached as /dev/disk/by-id/google-DEVICE_NAME')
+  )
   parser.add_argument(
       '--disk_size', action='store', required=False, default=200, type=int,
-      help='Boot disk size, in GB')
+      help='Boot disk size, in GB (Default: %(default)s)')
   parser.add_argument(
       '--instance_name', action='store', required=True, help='Name of instance')
   parser.add_argument(
       '--source_image', action='store', required=True,
       help='Path to the image, ie: /projects/<project_name>/zones/images/...')
   parser.add_argument(
-      '--linux_startup_script_url', action='store', required=False,
-      help='GCS url to a startup script for a Linux slave')
+      '--linux_startup_script_url', action='store', required=False, metavar=('SCRIPT_URL'),
+      help='GCS url to a startup script for a Linux instance')
   parser.add_argument(
       '--machine_type', action='store', required=False, default='n1-standard-8',
-      help='Type of machine')
+      help=('Type of machine (Default: "%(default)s)". For a list of valid '
+            'values, see https://cloud.google.com/compute/docs/machine-types'))
   parser.add_argument(
       '--network', action='store', required=False, default='default',
-      help='Type of network to use')
+      help='Type of network to use (Default: "%(default)s")')
   parser.add_argument(
       '--project', action='store', required=True, help='Name of the project')
   parser.add_argument(
-      '--ssh_pub_key', action='append', required=False,
+      '--ssh_public_key', action='append', required=False,
       help=('Specify SSH public keys to use. '
             'Example: \'root:ssh-rsa AAAA... root\''))
   parser.add_argument(
-      '--windows_startup_script_url', action='store', required=False,
-      help='GCS url to a startup script for a Windows slave')
+      '--windows_startup_script_url', action='store', required=False, metavar=('SCRIPT_URL'),
+      help='GCS url to a startup script for a Windows instance')
   parser.add_argument(
       '--zone', action='store', required=True, help='The zone for the instance')
 
@@ -198,8 +201,7 @@ if __name__ == '__main__':
 
   instance_metadata = None
 
-  manager = SlaveManager(
-      project=flags.project, zone=flags.zone, debug=flags.debug)
+  manager = SlaveManager(project=flags.project, zone=flags.zone)
 
   instance_metadata = {'items': []}
 
@@ -217,24 +219,34 @@ if __name__ == '__main__':
     }
     instance_metadata['items'].append(startup_item)
 
-  if flags.ssh_pub_key:
+  if flags.ssh_public_key:
     ssh_key_item = {
         'key': 'ssh-keys',
-        'value': '\n'.join(flags.linux_startup_script_url)
+        'value': '\n'.join(flags.ssh_public_key)
     }
     instance_metadata['items'].append(ssh_key_item)
 
+  persistent_disks = {}
+  pd_name = flags.attach_persistent_disk
+  if pd_name:
+    persistent_disks[pd_name] = pd_name
+  if flags.attach_persistent_disk_with_name:
+    pd_name, device_name = flags.attach_persistent_disk_with_name.split(':')
+    persistent_disks[device_name] = pd_name
+
   try:
     manager.CreateInstance(
-        flags.instance_name, persistent_disks=flags.attach_persistent_disk,
+        flags.instance_name, persistent_disks=persistent_disks,
         source_image=flags.source_image, machinetype=flags.machine_type,
         metadata=instance_metadata, network=flags.network)
   except apierrors.HttpError as error:
-    if error.resp['status'] == '409':
-      error_message = (
-          'There is already an instance names {0:s} in project {1:s}'.format(
-              flags.instance_name, flags.project)
-      )
+    error_dict = json.loads(error.content)
+    status = error_dict['error'].get('code', None)
+    error_message = error_dict['error'].get('message', '')
+    if status == 409 and error_message.endswith('already exists'):
+      print error_message
+    if status == 400 and error_message.endswith(
+        'The referenced image resource cannot be found.'):
       print error_message
     else:
       raise error
